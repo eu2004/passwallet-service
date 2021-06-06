577f7d1fa4693ad9010733e1e0e82c37ca3254a3
Emilian Utma <utma.emilian@gmail.com>
2021-06-06 09:28:20 +0300

    Update to AES-256 encryption

--- src/main/java/ro/eu/passwallet/service/crypt/AESHelper.java
+++ src/main/java/ro/eu/passwallet/service/crypt/AESHelper.java
@@ -5,17 +5,26 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
-public final class AESHelper {
+class AESHelper {
+    private final String messageDigestAlgorithm;
+    private final String secretKeyAlgorithm;
+
+    public AESHelper() {
+        this("SHA3-512", "AES");
+    }
+
+    protected AESHelper(String messageDigestAlgorithm, String secretKeyAlgorithm) {
+        this.messageDigestAlgorithm = messageDigestAlgorithm;
+        this.secretKeyAlgorithm = secretKeyAlgorithm;
+    }
 
     private SecretKeySpec generateKey(String password) {
         try {
             byte[] key = password.getBytes(StandardCharsets.UTF_8);
-            MessageDigest sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16);
-            return new SecretKeySpec(key, "AES");
+            MessageDigest sha = MessageDigest.getInstance(messageDigestAlgorithm);
+            byte[] secretKey = getSecretKey(sha.digest(key));
+            return new SecretKeySpec(secretKey, secretKeyAlgorithm);
         } catch (NoSuchAlgorithmException e) {
             throw new CryptographyException(e);
         }
@@ -24,7 +33,7 @@ public final class AESHelper {
     public byte[] encrypt(byte[] strToEncrypt, String secret) {
         try {
             SecretKeySpec secretKey = generateKey(secret);
-            Cipher cipher = Cipher.getInstance("AES");
+            Cipher cipher = Cipher.getInstance(secretKeyAlgorithm);
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return cipher.doFinal(strToEncrypt);
         } catch (Exception e) {
@@ -35,12 +44,23 @@ public final class AESHelper {
     public byte[] decrypt(byte[] strToDecrypt, String secret) {
         try {
             SecretKeySpec secretKey = generateKey(secret);
-            Cipher cipher = Cipher.getInstance("AES");
+            Cipher cipher = Cipher.getInstance(secretKeyAlgorithm);
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return cipher.doFinal(strToDecrypt);
         } catch (Exception e) {
             throw new CryptographyException(e);
         }
+    }
+
+    protected byte[] getSecretKey(byte[] hashKey) {
+        byte[] secretKey = new byte[hashKey.length / 2];
+        int j = 0;
+        for (int i = 0; i < hashKey.length; i++) {
+            if (i % 2 == 0) {
+                secretKey[j++] = hashKey[i];
+            }
+        }
+        return secretKey;
     }
 
 }
