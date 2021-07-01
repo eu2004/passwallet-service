f66f6e5a9c8e38ec4dc7e40dcd327ea0ffdb275e
Emilian Utma <utma.emilian@gmail.com>
2021-07-01 13:12:45 +0300

    Added more unit tests

--- src/test/java/ro/eu/passwallet/service/PasswordGeneratorTest.java
+++ src/test/java/ro/eu/passwallet/service/PasswordGeneratorTest.java
@@ -7,16 +7,19 @@ import org.junit.jupiter.api.Test;
 public class PasswordGeneratorTest {
 
     @Test
-    public void testGenerateLength() {
+    public void testDefaultGenerate() {
         //given
         PasswordGenerator passwordGenerator = new PasswordGenerator();
-        passwordGenerator.setLength(6);
 
         //when
         String pass = passwordGenerator.generate();
 
         //then
         Assertions.assertEquals(passwordGenerator.getLength(), pass.length());
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.LOWERCASE.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.UPPERCASE.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.NUMBERS.toCharArray()));
+        Assertions.assertFalse(charactersFound(pass, PasswordGenerator.SYMBOLS.toCharArray()));
     }
 
     @Test
@@ -30,13 +33,36 @@ public class PasswordGeneratorTest {
 
         //then
         Assertions.assertEquals(passwordGenerator.getLength(), pass.length());
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.LOWERCASE.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.UPPERCASE.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.NUMBERS.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.SYMBOLS.toCharArray()));
+    }
+
+    @Test
+    public void testGenerateLength() {
+        //given
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        passwordGenerator.setLength(6);
+
+        //when
+        String pass = passwordGenerator.generate();
+
+        //then
+        Assertions.assertEquals(passwordGenerator.getLength(), pass.length());
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.LOWERCASE.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.UPPERCASE.toCharArray()));
+        Assertions.assertTrue(charactersFound(pass, PasswordGenerator.NUMBERS.toCharArray()));
+    }
+
+    private boolean charactersFound(final String pass, final char[] chars) {
         boolean found = false;
-        for (char character : PasswordGenerator.SYMBOLS.toCharArray()) {
+        for (char character : chars) {
             found = pass.contains(String.valueOf(character));
             if (found) {
                 break;
             }
         }
-        Assertions.assertTrue(found);
+        return found;
     }
 }
